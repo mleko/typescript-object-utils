8aba5d638aa4c6fabca911ad9b38dbf89645e19c
Daniel Król <daniel@krol.me>
2017-01-28 11:06:45 +0100

    Export merge

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import "es6-shim";
 
 export {shallowEquals} from "./src/shallowEquals";
+export {merge} from "./src/merge";
 export {shallowMerge} from "./src/shallowMerge";
 export {mergeDeep, shallowMergeDeep} from "./src/mergeDeep";
