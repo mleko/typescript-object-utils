a2e6d63543f51442657477a46c34dd1d18e02727
Daniel Król <daniel@krol.me>
2017-08-08 19:32:24 +0200

    Allow to discard many keys

--- src/discard.test.ts
+++ src/discard.test.ts
@@ -9,5 +9,7 @@ test("Test discarding", (t) => {
 	t.deepEquals(discard(base, "b"), {a: 1, c: base.c});
 	t.notEquals(discard(base, "b").c, {});
 
+	t.deepEquals(discard(base, ["b", "c"]), {a: 1});
+
 	t.end();
 });
--- src/discard.ts
+++ src/discard.ts
@@ -1,8 +1,23 @@
-export function discard<A>(a: A, keyToDiscard: string): A {
+export function discard<A>(a: A, toDiscard: string | string[]): A {
+	return typeof toDiscard === "string" ? discardSingle(a, toDiscard) : discardMany(a, toDiscard as string[]);
+}
+
+function discardSingle<A>(a: A, toDiscard: string): A {
 	const result = {};
 	const keys = Object.keys(a);
 	for (const key of keys) {
-		if (key != keyToDiscard && a.hasOwnProperty(key)) {
+		if (key !== toDiscard && a.hasOwnProperty(key)) {
+			result[key] = a[key];
+		}
+	}
+	return result as A;
+}
+
+function discardMany<A>(a: A, toDiscard: string[]): A {
+	const result = {};
+	const keys = Object.keys(a);
+	for (const key of keys) {
+		if (-1 === toDiscard.indexOf(key) && a.hasOwnProperty(key)) {
 			result[key] = a[key];
 		}
 	}
